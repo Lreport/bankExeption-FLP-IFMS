c4796f802a67cdd246de61d5d4746557a9b11a9e
Report <tutuiga@gmail.com>
2025-11-24 15:38:00 -0300

    Create __init__.py

---
+++ models/abstratas/__init__.py
@@ -0,0 +1,6 @@
+"""Interfaces abstratas utilizadas pelos modelos."""
+
+from .Autenticavavel import Autenticavel as Autenticavavel
+from .Rentavel import Rentavel
+from .Tributavel import Tributavel
+
